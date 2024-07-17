952270162954ea0e1916b0ff13f75133fd2f90d2
Murilo Alexandre <52869210+muriloxx@users.noreply.github.com>
2024-07-17 20:55:02 -0300

    Add files via upload

---
+++ FixSalario/FixSalario/Funcionario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixSalario
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public double AumentarSalario(double porcentagem)
+        {
+            return SalarioBruto= SalarioBruto + (SalarioBruto * porcentagem / 100);
+        }
+
+        public override string ToString()
+        {
+            return Nome
+            + ", $"
+            + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+    }
+}
---
+++ FixSalario/FixSalario/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace FixSalario;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Funcionario p = new Funcionario();
+        Console.WriteLine("Nome: ");
+        p.Nome = Console.ReadLine();
+        Console.WriteLine("Salário Bruto: " );
+        p.SalarioBruto = double.Parse (Console.ReadLine(), CultureInfo.InvariantCulture );
+        Console.WriteLine("Imposto: ");
+        p.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine();
+        Console.Write("Funcionário: " + p);
+        Console.WriteLine();
+
+        Console.WriteLine("Digite a porcentagem para aumentar o salario: ");
+        double por = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        p.AumentarSalario(por);
+
+        Console.WriteLine();
+        Console.Write("Dados Atualizados " + p);
+
+    }
+}
