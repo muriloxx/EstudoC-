01dc4a47530285af4d35bf71cfaa7c0b98696813
Murilo Alexandre <52869210+muriloxx@users.noreply.github.com>
2024-07-17 21:00:51 -0300

    Add files via upload

---
+++ Exercicios2/Exercicios2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace exercicios2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero inteiro:");
+            int n1 = int.Parse(Console.ReadLine());
+
+            if (n1 < 0)
+            {
+                Console.WriteLine("Negativo");
+            }
+            else
+            {
+                Console.WriteLine("Não Negativo");
+            }
+
+            Console.WriteLine("---------------------");
+
+            Console.WriteLine("Digite um numero inteiro:");
+            int n2 = int.Parse(Console.ReadLine());
+
+            if (n2 % 2 == 0 )
+            {
+                Console.WriteLine("PAR");
+            }
+            else
+            {
+                Console.WriteLine("impar");
+            }
+
+            Console.WriteLine("---------------------");
+
+            Console.WriteLine("Digite um numero a:");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite um numero b:");
+            int b = int.Parse(Console.ReadLine());
+
+            if(a % 2 == 0 && b % 2 == 0)
+            {
+                Console.WriteLine("São Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Não são multiplos");
+            }
+        }
+    }
+}
---
+++ Exercicios3/Exercicios3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicios3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha Suas Preferencias");
+            Console.WriteLine("1 - Álcool");
+            Console.WriteLine("2 - Gasolina");
+            Console.WriteLine("3 - Diesel");
+            Console.WriteLine("4 - Fim");
+            int escolha = int.Parse(Console.ReadLine());
+            int alcool = 0;
+            int Gasolina = 0;
+            int Diesel = 0;
+            while (escolha != 4)
+            {
+                if (escolha == 1)
+                {
+                    alcool++;
+                }
+               if (escolha == 2)
+                {
+                    Gasolina++;
+                }
+                if (escolha == 3)
+                {
+                    Diesel++;
+                }
+
+                else if (escolha > 4 || escolha < 0)
+                { 
+                    Console.WriteLine("Codigo invalido Digite novo codigo");
+                   
+                }
+                escolha = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("Muito Obrigado");
+            Console.WriteLine("Alcool: " + alcool);
+            Console.WriteLine("Gasolina: " + Gasolina);
+            Console.WriteLine("Diesel: " + Diesel);
+        }
+    }
+}
